f2844430a577b1f77c37f4a439aa005dcb57b951
jaquinox <jaquinox123@gmail.com>
2024-06-01 18:08:10 -0400

    terminar 2

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
+from django.views.generic import ListView
 #from django.template import TemplateView
 from .models import Post
 
 # Create your views here.
-
+""""
 class HomePageView(TemplateView):
+    template_name = "home.html"
+    model = Post
+"""
+class HomePageView(ListView):
     template_name = "home.html"
     model = Post
